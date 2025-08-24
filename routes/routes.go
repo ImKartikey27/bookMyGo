ab2b8372a089bccc4bbb84c2edc230c623941fd3
ImKartikey27 <kartikey.22b1531008@abes.ac.in>
2025-08-24 11:49:39 +0530

    UPDT: routes

--- routes/routes.go
+++ routes/routes.go
@@ -8,6 +8,8 @@ import (
 func SetupRoutes(r *gin.Engine){
 	theaterController := controllers.NewTheaterController()
 	movieController := controllers.NewMovieController()
+	bookingController := controllers.NewBookingController()
+	showController := controllers.NewShowController()
 
 	api := r.Group("/api/v1")
 	{
@@ -27,5 +29,27 @@ func SetupRoutes(r *gin.Engine){
 			movies.PUT("/:id", movieController.UpdateMovie)
 			movies.DELETE("/:id", movieController.DeleteMovie)
 		}
+
+		booking := api.Group("/bookings")
+		{
+			booking.POST("/", bookingController.CreateBooking)
+			booking.GET("/", bookingController.GetBookingByID)
+			booking.GET("/:showID", bookingController.GetBookingsByShow)
+			booking.DELETE("/:id", bookingController.CancelBooking)
+			booking.GET("/check-availability", bookingController.CheckSeatAvailability)
+			booking.GET("/available-seats/:showID", bookingController.GetAvailableSeats)
+
+		}
+		shows := api.Group("/shows")
+		{
+			shows.POST("/", showController.CreateShow)
+			shows.GET("/", showController.GetAllShows)
+			shows.GET("/:id", showController.GetShowByID)
+			shows.GET("/movie/:movieID", showController.GetShowsByMovie)
+			shows.GET("/theater/:theaterID",showController.GetShowsByTheater)
+			shows.DELETE("/:id", showController.DeleteShow)
+		}
+
+		
 	}
 }
